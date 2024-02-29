007aff0d717609863f56e1a70645070ff34b88b9
Alfredo <21030761@itcelaya.edu.mx>
2024-02-29 08:04:49 -0600

    NavBar new style

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,21 +3,21 @@ import React from 'react'
 
 export const Navbar = () => {
     return (
-        <nav className="Navbar flex align-center bg-black ">
+        <nav className="Navbar flex align-center">
             <img className='Itcoders-Icon' src="https://i.ibb.co/Hhgt04B/itcoders-icon-removebg-preview.png" alt="Logo Itcoders" />
             <div>
                 <ul className="Navbar-List flex">
                     <a href="#About-Us-Section">
-                        <li>Sobre nosotros</li>
                         <i className="fa-solid fa-circle-info"></i>
+                        <li>Sobre nosotros</li>
                     </a>
                     <a href="#Our-Projects-Section">
-                        <li>Proyectos</li>
                         <i className="fa-solid fa-folder"></i>
+                        <li>Proyectos</li>
                     </a>
                     <a href="#Contact">
-                        <li>Contacto</li>
                         <i className="fa-solid fa-phone"></i>
+                        <li>Contacto</li>
                     </a>
                 </ul>
             </div>
