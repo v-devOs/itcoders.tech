c51d825b1e1bd69ca8f683933002fbb8a7e59163
Alfredo <21030761@itcelaya.edu.mx>
2024-03-06 18:34:44 -0600

    Home UI demo. No responsive.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { OurProjects, InitialPlayer } from "@/components/landing-page";
 import { ProjectCard } from "@/components/project-card/ProjectCard";
+import { ProjectCardHide } from "@/components/project-card/ProjectCard-hide";
+import { ProjectCardInfo } from "@/components/project-card/ProjectCard-info";
+import Link from "next/link";
 import { title } from "process";
 
 export default function Home() {
@@ -18,9 +21,12 @@ export default function Home() {
       </section> */}
 
       <section className="bg-white p-40">
-          <div className="Our-Projects-Slide width-100 flex justify-content align-center">
+          <div className="Our-Projects-Slide width-100 flex justify-content align-center gap-15">
             <ProjectCard></ProjectCard>
-            
+            <ProjectCardHide></ProjectCardHide>
+            <ProjectCardHide></ProjectCardHide>
+            <ProjectCardInfo></ProjectCardInfo>
+            <ProjectCardHide></ProjectCardHide>
           </div>
       </section>  
       
@@ -44,17 +50,21 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="bg-white flex justify-between p-40" style={{paddingTop: "10px"}}>
-        <div className="" style={{width: "20%"}}>
+      <section className="bg-white flex align-center space-between p-40" style={{paddingTop: "10px"}}>
+        <div className="" style={{width: "25%"}}>
           <p className="f-size-24">Nuestros números hablan</p>
+          <Link href='' className="bg-primary white-text ph-20 p-10 radius gap-15 flex align-center" style={{width: "60%"}}>
+                Cotizar
+                <i className="fa-solid fa-arrow-right"></i>
+          </Link>
         </div>
 
-        <div className="" style={{width: "80%"}}>
+        <div className="flex space-evenly" style={{width: "80%"}}>
           
           {/* TODO: Hacer componentes estas etiquetas  */}
           <div className="border-radius-30 p-10 bg-light-gray-color flex  align-center" style={{width: "180px", height:"75px"}}>
-            <div className="icon-size flex align-center justify-content border-radius-20 bg-primary mr-10">
-              <i className="fa-brands fa-github fa-xl white-text"></i>
+            <div className="border-white icon-size flex align-center justify-content  border-radius-20 bg-light-gray-color mr-10">
+              <i className="fa-brands fa-github fa-xl color-primary"></i>
             </div>
 
             <div >
@@ -63,86 +73,42 @@ export default function Home() {
             </div>
           </div>
 
-        </div>
-      </section>
+          <div className="border-radius-30 p-10 bg-light-gray-color flex  align-center" style={{width: "180px", height:"75px"}}>
+            <div className="border-white icon-size flex align-center justify-content  border-radius-20 bg-light-gray-color mr-10">
+              <i className="fa-thin fa-people-group fa-xl color-primary"></i>
+            </div>
 
-      {/* <section id="Our-Projects-Section" className='white-text bg-secondary p-40' style={{ paddingBottom: 300 }}>
-        <h2 className='f-size-50'>Nuestros proyectos</h2>
-        <OurProjects />
-      </section>
-      <section className='bg-thirt p-40 white-text'>
-        <h2 className='f-size-50'>Nuestros servicios</h2>
-        <div className='grid-c-4 gap-25 mt-50'>
-          <div>
-            <div className='center-text f-size-100'>
-              <i className="fa-solid fa-globe"></i>
+            <div >
+              <p className="f-size-14">Proyectos</p>
+              <p className="f-size-20">80</p>
             </div>
-            <h3 className='f-size-30 mt-20'>Sitios web</h3>
-            <p className='f-size-24'>Desarrollamos sitios web a la medida de tus necesidades.</p>
           </div>
-          <div>
-            <div className='center-text f-size-100'>
-              <i className="fa-solid fa-cart-shopping"></i>
-            </div>
-            <h3 className='f-size-30 mt-20'>Tiendas en línea</h3>
-            <p className='f-size-24'>Creamos tiendas en línea con servicios integrados y seguridad de datos.</p>
-          </div>
-          <div>
-            <div className='center-text f-size-100'>
-              <i className="fa-solid fa-computer"></i>
-            </div>
-            <h3 className='f-size-30 mt-20'>Sistemas</h3>
-            <p className='f-size-24'>Diseñamos y construimos sistemas a la medida de tus necesidades.</p>
-          </div>
-          <div>
-            <div className='center-text f-size-100'>
-              <i className="fa-solid fa-mobile-button"></i>
-            </div>
-            <h3 className='f-size-30 mt-20'>Aplicaciones móviles</h3>
-            <p className='f-size-24'>Desarrollamos aplicaciones móviles para Android y iOS.</p>
-          </div>
-        </div>
-      </section>
-       */}
-      {/* <section className='p-100 flex align-center justify-content'>
-        <div className='bg-white p-20'>
-          <h2 className='f-size-50'>Contacto</h2>
-          <form action="">
-            <input type="text" placeholder="Nombre"></input>
-            <input type="email" placeholder="Correo"></input>
-            <input type="text" placeholder="Asunto"></input>
-            <textarea name="mensaje" id="mensaje" placeholder="Mensaje"></textarea>
-            <input type="submit" value="Enviar" className="btn btn-primary" />
-          </form>
-        </div>
-      </section> */}
-      {/* <section id="Contact" className="login-box flex column white-text">
-        <h3 className="secondary-title-color">Si buscas una solución novedosa para tu negocio, ponte en contacto con nosotros</h3>
-        <form>
-          <div className="user-box">
-            <input type="text" placeholder="Nombre"></input>
-          </div>
-          <div className="user-box">
-            <input type="email" placeholder="Correo"></input>
-          </div>
-          <div className="user-box">
-            <input type="text" placeholder="Asunto"></input>
-          </div>
-          <a href="#">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            Enviar infromación
-          </a>
-        </form>
-      </section> */}
 
-      {/* <section className='white-text p-40 bg-secondary flex column justify-content align-center'>
-      {/* <section className='white-text p-40 bg-secondary flex column justify-content align-center'>
-        <h2 className='f-size-50'>Equipo</h2>
-        <img className="Team-Img" src="https://i.ibb.co/pxc297G/Whats-App-Image-2024-02-27-at-3-20-27-PM.jpg" alt="Equipo de trabajo" />
-      </section> */}
+          <div className="border-radius-30 p-10 bg-light-gray-color flex  align-center" style={{width: "180px", height:"75px"}}>
+            <div className="border-white icon-size flex align-center justify-content  border-radius-20 bg-light-gray-color mr-10">
+              <i className="fa-thin fa-chart-simple fa-xl color-primary"></i>
+            </div>
+
+            <div>
+              <p className="f-size-14">Clientes</p>
+              <p className="f-size-20">30</p>
+            </div>
+          </div>
+
+          <div className="border-radius-30 p-10 bg-light-gray-color flex  align-center" style={{width: "180px", height:"75px"}}>
+            <div className="border-white icon-size flex align-center justify-content  border-radius-20 bg-light-gray-color mr-10">
+              <i className="fa-thin fa-arrows-to-dot fa-xl color-primary"></i>
+            </div>
+
+            <div >
+              <p className="f-size-14">Clicks</p>
+              <p className="f-size-20">+300K</p>
+            </div>
+          </div>
+
+        </div>
+      </section>
+      
     </main>
 
   )
