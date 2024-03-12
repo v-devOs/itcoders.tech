79b01aa004707e5d2e357c50ed1d1c4223e02bac
agus022 <flores.jorge.1j@gmail.com>
2024-03-11 20:37:33 -0600

    add info blog pt.1

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -13,6 +13,8 @@ export const Navbar = () => {
                 <Link href='/sobre-nosotros' className={`ph-20 p-10 radius ${pathname == '/sobre-nosotros' ? 'bg-primary white-text' : 'black-text'}`} >Sobre nosotros</Link>
                 <Link href='/servicios' className={`ph-20 p-10 radius ${pathname == '/servicios' ? 'bg-primary white-text' : 'black-text'}`}>Servicios</Link>
                 <Link href='/contacto' className={`ph-20 p-10 radius ${pathname == '/contacto' ? 'bg-primary white-text' : 'black-text'}`}>Contacto</Link>
+                <Link href='/blog' className={`ph-20 p-10 radius ${pathname == '/blog' ? 'bg-primary white-text' : 'black-text'}`}>BOG</Link>
+
             </div>
             <Link href='' className="bg-primary white-text ph-20 p-10 radius gap-15 flex align-center" style={{ position: 'absolute', right: 100 }}>
                 Cotizar
